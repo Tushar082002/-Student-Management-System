a9cd75d0c0d2e28bf38fb0434bc1a9a65fd8be66
Tushar Arora <arorat802@gmail.com>
2025-06-30 14:40:16 +0530

    completed

--- src/main/java/com/example/studentapi/Repository/StudentRepository.java
+++ src/main/java/com/example/studentapi/Repository/StudentRepository.java
@@ -2,7 +2,9 @@ package com.example.studentapi.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.studentapi.model.Student;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
 }
